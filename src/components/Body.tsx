70e617b8d741efc90de755fcb18c362f12d3aa16
AksharPrajapati <aksharpp369@gmail.com>
2024-07-27 23:47:20 +0530

    feat: add formik validation

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Browse from "./Browse";
+import Header from "./Header";
 
 function Body() {
   return (
     <div className="h-screen">
+      <Header />
       <Browse />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out successful");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div className="absolute bg-gradient-to-b from-black w-full z-50">
+    <div className="absolute bg-gradient-to-b from-black w-full z-50 flex justify-between">
       <img
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
         className="w-44 ml-28 mt-4"
       />
+      <button
+        className="bg-red-600 px-6 py-1 mr-6 my-6 rounded text-white font-medium tracking-wide hover:bg-red-700"
+        onClick={handleLogout}
+      >
+        Sign out
+      </button>
     </div>
   );
 }
