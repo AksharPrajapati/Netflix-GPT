283d6cb504ca1551483ef926428b59904d63e149
Akshar Prajapati <aksharpp369@gmail.com>
2026-06-11 21:01:41 +0530

    fix: update Firebase configuration to use environment variables

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,23 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBcqD67Svg1fxFcVzY3TzQwiFbUbzYQadA",
-  authDomain: "netflix-gpt-a0b46.firebaseapp.com",
-  projectId: "netflix-gpt-a0b46",
-  storageBucket: "netflix-gpt-a0b46.appspot.com",
-  messagingSenderId: "195127816614",
-  appId: "1:195127816614:web:f411d5435e61c00b55df67",
-  measurementId: "G-K3YNCZYQY0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
+export const auth = getAuth(app);
